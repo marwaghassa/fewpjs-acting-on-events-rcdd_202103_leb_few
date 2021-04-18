035054fceae89bb1579bd706b6fcea5e233d9b4c
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-18 14:47:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ let dodger=document.getElementById('dodger');
 document.addEventListener("Keydown", function(e){
   
   if(e.Key==="ArrowLeft"){
-    let leftNumbers=dodger.style.left.replace("px","");
+    let leftNumbers=dodger.style.left.replace("px","");//
     let left=parseInt(leftNumbers,10);
+    dodge.style.left='${left}-1';
     
   }
 }
