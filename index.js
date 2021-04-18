162e1b5cff22ce5c10f1263552ac3ccb154aeca5
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-18 15:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function moveDodgerRight() {
   let right = parseInt(leftNumbers, 10);
  
   if (right > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.right = `${right+1}px`;
   }
 };
