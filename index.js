428718f5b75ac942e4a93ff051ca65747f1eee46
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-18 14:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 // Your code here
 let dodger=document.getElementById('dodger');
 
-document.addEventListener("Keydown", function(e){
-  
-  if(e.Key==="ArrowLeft"){
-    let leftNumbers=dodger.style.left.replace("px","");//
-    let left=parseInt(leftNumbers,10);
-    dodge.style.left='${left}-1';
-    
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
 }
-
-
-
-
-);
