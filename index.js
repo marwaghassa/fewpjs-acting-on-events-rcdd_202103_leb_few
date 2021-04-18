9aa824aac27577f9a189b0450ae5b45ef4f249c4
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-18 14:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,16 +2,4 @@
 let dodger=document.getElementById('dodger');
 
 
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
- 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
-  }
-};
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-});
+
